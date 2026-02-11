8a8d50dd30cbef276a438a020a20618ed69b5415
Nikhil Jangir <nikhiljangir@Nikhils-MacBook-Air.local>
2026-02-10 18:49:44 -0600

    hw1 complete

---
+++ agents.py
@@ -0,0 +1,25 @@
+
+
+class Agent: 
+
+    def __init__(self):
+        self.carrying_box = False
+        self.at_household = False
+
+    loc_A = "Agent"
+    loc_B = "Household"
+
+    
+class Environment: 
+
+    None
+
+
+class RandomAgentProgram:
+
+    None
+
+
+class TraceAgent: 
+
+    None
